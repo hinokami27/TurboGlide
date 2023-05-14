044df8fe0c7301856775b2b20fa6ae1f127708d2
Dragan Golaboski <dragangolab@gmail.com>
2023-05-14 18:04:12 +0200

    Added Puck movement and collision

--- GameWindow.Designer.cs
+++ GameWindow.Designer.cs
@@ -34,16 +34,22 @@
             this.pbPlayerA = new System.Windows.Forms.PictureBox();
             this.pbPuck = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pbGoalB = new System.Windows.Forms.PictureBox();
+            this.pbGoalA = new System.Windows.Forms.PictureBox();
+            this.lbPointsA = new System.Windows.Forms.Label();
+            this.lbPointsB = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlayerB)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlayerA)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPuck)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGoalB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGoalA)).BeginInit();
             this.SuspendLayout();
             // 
             // pbPlayerB
             // 
             this.pbPlayerB.BackColor = System.Drawing.Color.Transparent;
             this.pbPlayerB.Image = ((System.Drawing.Image)(resources.GetObject("pbPlayerB.Image")));
-            this.pbPlayerB.Location = new System.Drawing.Point(200, 628);
+            this.pbPlayerB.Location = new System.Drawing.Point(219, 375);
             this.pbPlayerB.Name = "pbPlayerB";
             this.pbPlayerB.Size = new System.Drawing.Size(66, 66);
             this.pbPlayerB.TabIndex = 0;
@@ -53,7 +59,7 @@
             // 
             this.pbPlayerA.BackColor = System.Drawing.Color.Transparent;
             this.pbPlayerA.Image = ((System.Drawing.Image)(resources.GetObject("pbPlayerA.Image")));
-            this.pbPlayerA.Location = new System.Drawing.Point(200, 79);
+            this.pbPlayerA.Location = new System.Drawing.Point(200, 220);
             this.pbPlayerA.Name = "pbPlayerA";
             this.pbPlayerA.Size = new System.Drawing.Size(66, 66);
             this.pbPlayerA.TabIndex = 1;
@@ -63,7 +69,7 @@
             // 
             this.pbPuck.BackColor = System.Drawing.Color.Transparent;
             this.pbPuck.Image = ((System.Drawing.Image)(resources.GetObject("pbPuck.Image")));
-            this.pbPuck.Location = new System.Drawing.Point(208, 359);
+            this.pbPuck.Location = new System.Drawing.Point(300, 664);
             this.pbPuck.Name = "pbPuck";
             this.pbPuck.Size = new System.Drawing.Size(50, 50);
             this.pbPuck.TabIndex = 2;
@@ -74,24 +80,68 @@
             this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pbGoalB
+            // 
+            this.pbGoalB.BackColor = System.Drawing.Color.Transparent;
+            this.pbGoalB.Location = new System.Drawing.Point(171, 709);
+            this.pbGoalB.Name = "pbGoalB";
+            this.pbGoalB.Size = new System.Drawing.Size(124, 28);
+            this.pbGoalB.TabIndex = 3;
+            this.pbGoalB.TabStop = false;
+            // 
+            // pbGoalA
+            // 
+            this.pbGoalA.BackColor = System.Drawing.Color.Transparent;
+            this.pbGoalA.Location = new System.Drawing.Point(171, 30);
+            this.pbGoalA.Name = "pbGoalA";
+            this.pbGoalA.Size = new System.Drawing.Size(124, 28);
+            this.pbGoalA.TabIndex = 4;
+            this.pbGoalA.TabStop = false;
+            // 
+            // lbPointsA
+            // 
+            this.lbPointsA.AutoSize = true;
+            this.lbPointsA.Location = new System.Drawing.Point(13, 13);
+            this.lbPointsA.Name = "lbPointsA";
+            this.lbPointsA.Size = new System.Drawing.Size(0, 13);
+            this.lbPointsA.TabIndex = 5;
+            // 
+            // lbPointsB
+            // 
+            this.lbPointsB.AutoSize = true;
+            this.lbPointsB.Location = new System.Drawing.Point(397, 9);
+            this.lbPointsB.Name = "lbPointsB";
+            this.lbPointsB.Size = new System.Drawing.Size(0, 13);
+            this.lbPointsB.TabIndex = 6;
+            // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(466, 764);
+            this.Controls.Add(this.lbPointsB);
+            this.Controls.Add(this.lbPointsA);
+            this.Controls.Add(this.pbGoalA);
+            this.Controls.Add(this.pbGoalB);
             this.Controls.Add(this.pbPuck);
             this.Controls.Add(this.pbPlayerA);
             this.Controls.Add(this.pbPlayerB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(482, 803);
             this.Name = "GameWindow";
-            this.Text = "GameWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TurboGlide";
             this.Load += new System.EventHandler(this.GameWindow_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameWindow_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameWindow_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pbPlayerB)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPlayerA)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPuck)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGoalB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGoalA)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,5 +151,9 @@
         private System.Windows.Forms.PictureBox pbPlayerA;
         private System.Windows.Forms.PictureBox pbPuck;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbGoalB;
+        private System.Windows.Forms.PictureBox pbGoalA;
+        private System.Windows.Forms.Label lbPointsA;
+        private System.Windows.Forms.Label lbPointsB;
     }
 }
--- GameWindow.cs
+++ GameWindow.cs
@@ -12,102 +12,165 @@ namespace TurboGlide
 {
     public partial class GameWindow : Form
     {
-        bool goUp1;
-        bool goDown1;
-        bool goLeft1;
-        bool goRight1;
+        bool goUpA;
+        bool goDownA;
+        bool goLeftA;
+        bool goRightA;
 
-        bool goUp2;
-        bool goDown2;
-        bool goLeft2;
-        bool goRight2;
+        bool goUpB;
+        bool goDownB;
+        bool goLeftB;
+        bool goRightB;
 
-        int p1Speed = 5;
-        int p2Speed = 5;
+        int p1Speed = 10;
+        int p2Speed = 10;
+        int puckSpeedX= 5; 
+        int puckSpeedY= 5;
+
+        int PlayerAPoints = 0;
+        int PlayerBPoints = 0;
+        Random rnd = new Random();
+        public int randomNum { get; set; }
+
 
         public GameWindow()
         {
             InitializeComponent();
             timer1.Start();
             DoubleBuffered = true;
+            
         }
 
         private void GameWindow_Load(object sender, EventArgs e)
         {
-
+            pbPlayerA.Location = new Point(200, 65);
+            pbPlayerB.Location = new Point(200, 638);
+            pbPuck.Location = new Point(208, 357);
+            pbGoalB.Location = new Point(171, 709);
+            pbGoalA.Location = new Point(171, 30);
+            randomNum = rnd.Next();
+            Console.WriteLine(randomNum.ToString());
+            lbPointsA.Text = "Plaeyr A: ";
+            lbPointsB.Text = "Player B: ";
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if(goUp1 == true)
+            if(goUpA == true && pbPlayerA.Top > 55)
             {
                 pbPlayerA.Top -= p1Speed;
             }
-            if(goDown1 == true)
+            if(goDownA == true && pbPlayerA.Top < 315)
             {
                 pbPlayerA.Top += p1Speed;
             }
-            if(goLeft1 == true)
+            if(goLeftA == true && pbPlayerA.Left > 60)
             {
                 pbPlayerA.Left -= p1Speed;
             }
-            if(goRight1 == true)
+            if(goRightA == true && pbPlayerA.Left < 340)
             {
                 pbPlayerA.Left += p1Speed;
             }
 
-            if (goUp2 == true)
+            if (goUpB == true && pbPlayerB.Top > 388)
             {
                 pbPlayerB.Top -= p2Speed;
             }
-            if (goDown2 == true)
+            if (goDownB == true && pbPlayerB.Top < 648)
             {
                 pbPlayerB.Top += p2Speed;
             }
-            if (goLeft2 == true)
+            if (goLeftB == true && pbPlayerB.Left > 60)
             {
                 pbPlayerB.Left -= p2Speed;
             }
-            if (goRight2 == true)
+            if (goRightB == true && pbPlayerB.Left < 340)
             {
                 pbPlayerB.Left += p2Speed;
             }
+            //Puck slide at GameStart to avoid 2player 1puck collision
+            if (randomNum % 2 == 0)
+            {
+                pbPuck.Top -= puckSpeedY;
+                pbPuck.Left += puckSpeedX;
+
+            }
+            else
+            {
+                pbPuck.Top += puckSpeedY;
+                pbPuck.Left += puckSpeedX;
+            }
+            if(pbPuck.Left < 60 || pbPuck.Left > 340)
+            {
+                puckSpeedX = -puckSpeedX;
+            }
+            if(pbPuck.Top < 55 || pbPuck.Top > 664)
+            {
+                puckSpeedY = -puckSpeedY;
+            }
+            if (pbPuck.Bounds.IntersectsWith(pbPlayerA.Bounds))
+            {
+                puckSpeedY = -puckSpeedY;
+              
+
+            }
+            if (pbPuck.Bounds.IntersectsWith(pbPlayerB.Bounds))
+            {
+                puckSpeedY = -puckSpeedY;
+               
+            }
+            if(pbPuck.Bounds.IntersectsWith(pbGoalA.Bounds))
+            {
+                PlayerBPoints+=1;
+                pbPuck.Location = new Point(208, 357);
+                lbPointsB.Text = "Player B: " +PlayerBPoints.ToString();
+
+            }
+            if (pbPuck.Bounds.IntersectsWith(pbGoalB.Bounds))
+            {
+                PlayerAPoints += 1;
+                pbPuck.Location = new Point(208, 357);
+                lbPointsA.Text = "Player A: " + PlayerAPoints.ToString();
+
+            }
+
         }
 
         private void GameWindow_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.W)
             {
-                goUp1 = true;
+                goUpA = true;
             }
             if (e.KeyCode == Keys.S)
             {
-                goDown1 = true;
+                goDownA = true;
             }
             if (e.KeyCode == Keys.A)
             {
-                goLeft1 = true;
+                goLeftA = true;
             }
             if (e.KeyCode == Keys.D)
             {
-                goRight1 = true;
+                goRightA = true;
             }
 
             if (e.KeyCode == Keys.Up)
             {
-                goUp2 = true;
+                goUpB = true;
             }
             if (e.KeyCode == Keys.Down)
             {
-                goDown2 = true;
+                goDownB = true;
             }
             if (e.KeyCode == Keys.Left)
             {
-                goLeft2 = true;
+                goLeftB = true;
             }
             if (e.KeyCode == Keys.Right)
             {
-                goRight2 = true;
+                goRightB = true;
             }
 
         }
@@ -116,36 +179,36 @@ namespace TurboGlide
         {
             if (e.KeyCode == Keys.W)
             {
-                goUp1 = false;
+                goUpA = false;
             }
             if (e.KeyCode == Keys.S)
             {
-                goDown1 = false;
+                goDownA = false;
             }
             if (e.KeyCode == Keys.A)
             {
-                goLeft1 = false;
+                goLeftA = false;
             }
             if (e.KeyCode == Keys.D)
             {
-                goRight1 = false;
+                goRightA = false;
             }
 
             if (e.KeyCode == Keys.Up)
             {
-                goUp2 = false;
+                goUpB = false;
             }
             if (e.KeyCode == Keys.Down)
             {
-                goDown2 = false;
+                goDownB = false;
             }
             if (e.KeyCode == Keys.Left)
             {
-                goLeft2 = false;
+                goLeftB = false;
             }
             if (e.KeyCode == Keys.Right)
             {
-                goRight2 = false;
+                goRightB = false;
             }
         }
     }
