63e6a77019cbe3557a00451f8116eda2ebff35bb
Dragan Golaboski <dragangolab@gmail.com>
2023-05-16 16:22:14 +0200

    Added victory screens, controls backround images

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.btnStartGame.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.btnStartGame.Font = new System.Drawing.Font("Noto Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStartGame.ForeColor = System.Drawing.Color.Black;
-            this.btnStartGame.Location = new System.Drawing.Point(120, 475);
+            this.btnStartGame.Location = new System.Drawing.Point(120, 460);
             this.btnStartGame.Name = "btnStartGame";
             this.btnStartGame.Size = new System.Drawing.Size(226, 75);
             this.btnStartGame.TabIndex = 0;
